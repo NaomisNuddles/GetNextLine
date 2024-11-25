6986f9de4d7cc6073ccf03bc40d35176584170e7
NaomisNuddles <nmla0012@gmail.com>
2024-11-25 10:28:29 +0000

    begin project

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nleandro <nleandro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 10:27:31 by nleandro          #+#    #+#             */
+/*   Updated: 2024/11/25 10:27:31 by nleandro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+//get next line
+char		*get_next_line(int fd);
+
+//utils
+#endif //GET_NEXT_LINE_H
